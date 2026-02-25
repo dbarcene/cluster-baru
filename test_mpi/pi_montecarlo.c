77934a6a53bc11dde7a7259022b60d2a742d7a1b
dbarcene <david.barcene@ut.ac.pa>
2026-02-25 11:18:14 -0500

    mpi testing with montecarlo

---
+++ test_mpi/pi_montecarlo.c
@@ -0,0 +1,9 @@
+/**
+ * File              : pi_montecarlo.c
+ * Author            : David Barcene <dbarcene@indicasat.org.pa>
+ * Date              : 25.02.2026
+ * Last Modified Date: 25.02.2026
+ * Last Modified By  : David Barcene <dbarcene@indicasat.org.pa>
+ */
+
+
